d03bb497acaf0f751fe1b0816eaf58957a248dfc
1103208228 <97216586+1103208228@users.noreply.github.com>
2022-01-10 01:26:07 +0700

    new

--- src/Page/Login.js
+++ src/Page/Login.js
@@ -26,10 +26,8 @@ const Login = () => {
         setLoading(false)
     } 
     return (
-        <Container fluid className='m-0 align-items-center p-0 h-100 w-100'>
-            <Image className='position-absolute m-0' src={BG}/>
+        <Container fluid className='m-0 align-items-center p-5 h-100' style={{ backgroundImage: `url(${BG})` }}>
         <Container className='m-5 d-flex justify-content-center align-items-center p-5'>
-            
             <Card fluid className=" p-5 h-75 w-50 bg-opacity-50 bg-black rounded-3" > 
             <Image className='img-fluid w-25 h-25 align-self-center' src={Brand}/>
                 <h1 className='text-center fs-2 text-white'>LOGIN</h1>
